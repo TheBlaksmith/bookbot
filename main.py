7671500d05232ce3be8742edfca6912b62d43aed
TheBlaksmith <hectorcasas1914@gmail.com>
2025-03-11 19:13:46 -0700

    printing the number of words

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 get_book_text = ""
+
 with open("./books/frankenstein.txt") as f:
     get_book_text = f.read()
-print(get_book_text)
+    
+all_words = get_book_text.split()
+num_words = len(all_words)
+
+print(num_words)
